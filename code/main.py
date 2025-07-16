e3cb998a0166a97ad0122f0e88df50bd495d190f
JacksonSwich <timurhaybulov@gmail.com>
2025-07-16 14:05:35 +0300

    finish testing

--- code/main.py
+++ code/main.py
@@ -3,22 +3,24 @@ import requests
 def get_json_response():
     """Получаем ответ от сервера в виде json файла"""
     response = requests.get("https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json")
-    print(f"Ответ сервера - {response}")
-    return response.json()
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Ошибка! Ответ сервера - {response}")
 
 
-def main(person_gender, is_working):
+def find_hero(file, person_gender, is_working):
     """Главная функция вывода результата поиска"""
-    file = get_json_response()
+    #file = get_json_response() # получаем json файл
 
     highest_height = 0
 
     # мужчина - работает
-    if person_gender == "Male" and is_working == True:
-        for row in range(0, len(file)):
-            if file[row]['appearance']['gender']  == 'Male' and file[row]['work']['occupation'] != '-':
+    if person_gender == "Male" and is_working == True: # проверяем входные данные в функции
+        for row in range(0, len(file)): # цикл перебирает всех персонажей из json файла
+            if file[row]['appearance']['gender']  == 'Male' and file[row]['work']['occupation'] != '-': # проверяем значения в строке героя
 
-                height = file[row]['appearance']['height'][1]
+                height = file[row]['appearance']['height'][1] # получаем рост
 
                 # форматирование вывода переменной роста
                 if height[-2:] == "cm":
@@ -31,15 +33,18 @@ def main(person_gender, is_working):
                 else:
                     print("ERROR")
 
+                # ищем самого высокого персонажа из группы
                 if height > highest_height:
                     highest_height = height
                     highest_hero = f"ID: {file[row]['id']}; NAME: {file[row]['name']}; APPEARANCE: gender: {file[row]['appearance']['gender']}; WORK: {file[row]['work']['occupation']}; height: {file[row]['appearance']['height'][1]};"
 
+                # вывод всех подходящих из группы
                 print(f"ID: {file[row]['id']}; NAME: {file[row]['name']}; APPEARANCE: gender: {file[row]['appearance']['gender']}; WORK: {file[row]['work']['occupation']}; height: {file[row]['appearance']['height'][1]};")
             else:
                 continue
 
-        print(f"\nЭто самый большой персонаж: {highest_hero} \nЕго рост = {highest_height} см")
+        # результат
+        print(f"\nЭто самый большой персонаж: {highest_hero} \nЕго рост = {highest_height} см\n")
         return highest_hero, highest_height
 
     # мужчина - не работает
@@ -68,7 +73,7 @@ def main(person_gender, is_working):
             else:
                 continue
 
-        print(f"\nЭто самый большой персонаж: {highest_hero} \nЕго рост = {highest_height} см")
+        print(f"\nЭто самый большой персонаж: {highest_hero} \nЕго рост = {highest_height} см\n")
         return highest_hero, highest_height
 
     # женщина - работает
@@ -97,7 +102,7 @@ def main(person_gender, is_working):
             else:
                 continue
 
-        print(f"\nЭто самый большой персонаж: {highest_hero} \nЕе рост = {highest_height} см")
+        print(f"\nЭто самый большой персонаж: {highest_hero} \nЕе рост = {highest_height} см\n")
         return highest_hero, highest_height
 
     # женщина - не работает
@@ -126,33 +131,38 @@ def main(person_gender, is_working):
             else:
                 continue
 
-        print(f"\nЭто самый большой персонаж: {highest_hero} \nЕе рост = {highest_height} см")
+        print(f"\nЭто самый большой персонаж: {highest_hero} \nЕе рост = {highest_height} см\n")
         return highest_hero, highest_height
 
     else:
         print("ERROR")
 
 
-# глобальная программа
-print("Введите входные данные для поиска:")
+# Функция main; Запрашиваем данные
+def main():
+    print("Введите входные данные для поиска:")
 
-gender = input("Введите пол (мужской/женский): ")
-if gender.lower() == "мужской":
-    gender = "Male"
-elif gender.lower() == "женский":
-    gender = "Female"
-else:
-    print("Ошибка! Неверное значение переменной gender")
-    exit()
+    gender = input("Введите пол (мужской/женский): ")
+    if gender.lower() == "мужской":
+        gender = "Male"
+    elif gender.lower() == "женский":
+        gender = "Female"
+    else:
+        print("Ошибка! Неверное значение переменной gender")
+        exit()
 
-work = input("Наличие работы (да/нет): ")
-if work.lower() == "да":
-    work = True
-elif work.lower() == "нет":
-    work = False
-else:
-    print("Ошибка! Неверное значение переменной work")
-    exit()
+    work = input("Наличие работы (да/нет): ")
+    if work.lower() == "да":
+        work = True
+    elif work.lower() == "нет":
+        work = False
+    else:
+        print("Ошибка! Неверное значение переменной work")
+        exit()
+
+    print(find_hero(get_json_response(), gender, work)) # вывод результата функции поиска персонажей
 
 
-main(gender, work)
+# Глобальная программа
+if __name__ == '__main__':
+    main()
--- code/testing.py
+++ code/testing.py
@@ -1,4 +1,160 @@
-from main import main
+import pytest
+from unittest.mock import patch
 
-def test_positive_script():
-    assert main("мужской", True) ==
+from main import find_hero, main
+
+# создал тестовые данные, которые будут передаваться в функцию, для заполнения фикстуры
+test_json_data = [
+    { # мужчина - работает
+        "id": 1,
+        "name": "person 1",
+        "appearance":
+        {
+            "gender": "Male",
+            "race": "Human",
+            "height": ["6'8", "20 cm"]
+        },
+        "work":
+        {
+            "occupation": "author",
+            "base": "-"
+        }
+    },
+
+    { # мужчина - не работает
+        "id": 2,
+        "name": "person 2",
+        "appearance":
+        {
+            "gender": "Male",
+            "race": "Human",
+            "height": ["6'8", "2.5 meters"]
+        },
+        "work":
+        {
+            "occupation": "-",
+            "base": "-"
+        }
+    },
+
+    { # женщина - работает
+        "id": 3,
+        "name": "person 3",
+        "appearance":
+        {
+            "gender": "Female",
+            "race": "Human",
+            "height": ["6'8", "431 cm"]
+        },
+        "work":
+        {
+            "occupation": "Musician",
+            "base": "-"
+        }
+    },
+
+    { # мужчина - работает
+        "id": 4,
+        "name": "person 4",
+        "appearance":
+        {
+            "gender": "Male",
+            "race": "Human",
+            "height": ["6'9", "123 cm"]
+        },
+        "work":
+        {
+            "occupation": "adventurer",
+            "base": "-"
+        }
+    },
+
+    { # женщина - не работает
+        "id": 5,
+        "name": "person 5",
+        "appearance":
+        {
+            "gender": "Female",
+            "race": "Human",
+            "height": ["6'8", "203 cm"]
+        },
+        "work":
+        {
+            "occupation": "-",
+            "base": "-"
+        }
+    }
+]
+
+
+# фикстура, которая создается один раз для всей тестовой сессии
+@pytest.fixture(scope="session")
+def get_json_data():
+    json_data = test_json_data
+    return json_data
+
+# ОСНОВНОЕ тестирование функции по поиску самого высокого персонажа
+def test_find_hero(get_json_data):
+    assert find_hero(get_json_data, "Male", True) == ('ID: 4; NAME: person 4; APPEARANCE: gender: Male; WORK: adventurer; height: 123 cm;', 123)
+    assert find_hero(get_json_data, "Male", False) == ('ID: 2; NAME: person 2; APPEARANCE: gender: Male; WORK: -; height: 2.5 meters;', 250)
+    assert find_hero(get_json_data, "Female", True) == ('ID: 3; NAME: person 3; APPEARANCE: gender: Female; WORK: Musician; height: 431 cm;', 431)
+    assert find_hero(get_json_data, "Female", False) == ('ID: 5; NAME: person 5; APPEARANCE: gender: Female; WORK: -; height: 203 cm;', 203)
+
+
+# ПОБОЧНОЕ тестирование функций ввода входных данных
+def test_man_yes(capsys, mocker):
+    mocker.patch("builtins.input", side_effect=["мужской", "да"]) # входные данные для input()
+    main()
+    output = capsys.readouterr() # записываем весь вывод программы
+    assert "Ошибка! Неверное значение переменной gender" not in output.out # если сообщения об ошибки нет - тест прошел
+    assert "Ошибка! Неверное значение переменной work" not in output.out
+
+def test_man_no(capsys, mocker):
+    mocker.patch("builtins.input", side_effect=["мужской", "нет"])
+    main()
+    output = capsys.readouterr()
+    assert "Ошибка! Неверное значение переменной gender" not in output.out
+    assert "Ошибка! Неверное значение переменной work" not in output.out
+
+def test_woman_yes(capsys, mocker):
+    mocker.patch("builtins.input", side_effect=["женский", "да"])
+    main()
+    output = capsys.readouterr()
+    assert "Ошибка! Неверное значение переменной gender" not in output.out
+    assert "Ошибка! Неверное значение переменной work" not in output.out
+
+def test_woman_no(capsys, mocker):
+    mocker.patch("builtins.input", side_effect=["женский", "нет"])
+    main()
+    output = capsys.readouterr()
+    assert "Ошибка! Неверное значение переменной gender" not in output.out
+    assert "Ошибка! Неверное значение переменной work" not in output.out
+
+def test_register_negative_script(capsys, mocker):
+    mocker.patch("builtins.input", side_effect=["МуЖСКОй", "ДА"])
+    main()
+    output = capsys.readouterr()
+    assert "Ошибка! Неверное значение переменной gender" not in output.out
+    assert "Ошибка! Неверное значение переменной work" not in output.out
+
+def test_register_negative_script_2(capsys, mocker):
+    mocker.patch("builtins.input", side_effect=["женСКИЙ", "нЕТ"])
+    main()
+    output = capsys.readouterr()
+    assert "Ошибка! Неверное значение переменной gender" not in output.out
+    assert "Ошибка! Неверное значение переменной work" not in output.out
+
+def test_data_negative_script(capsys, mocker):
+    mocker.patch("builtins.input", side_effect=["asdasfasf", "да"])
+    with pytest.raises(SystemExit): # ловим выход программы, чтобы тест продолжился
+        main()
+    output = capsys.readouterr()
+    assert "Ошибка! Неверное значение переменной gender" in output.out
+
+def test_data_negative_script_2(capsys, mocker):
+    mocker.patch("builtins.input", side_effect=["мужской", "2322186493"])
+    with pytest.raises(SystemExit):
+        main()
+    output = capsys.readouterr()
+    assert "Ошибка! Неверное значение переменной work" in output.out
+
